e491c8fb90e5947c52a532db58a61b71d1764fe2
Kansei Kogami <k_kansei_k@yahoo.co.jp>
2018-07-28 13:55:27 +0900

    modify: Menu model

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,7 @@
 class Menu < ApplicationRecord
   has_many :order_details
   has_many :orders, throught: :order_details
+
+  # acvive_strage
+  has_one_attached :image
 end
---
+++ db/migrate/20180728041355_remove_image_from_menus.rb
@@ -0,0 +1,5 @@
+class RemoveImageFromMenus < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :menus, :image, :string
+  end
+end
