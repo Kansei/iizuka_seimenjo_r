160331144dbddafff783acd06ce857bbb1ae729a
Kansei Kogami <k_kansei_k@yahoo.co.jp>
2018-07-28 14:08:02 +0900

    fix: spell miss

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,6 @@
 class Menu < ApplicationRecord
   has_many :order_details
-  has_many :orders, throught: :order_details
+  has_many :orders, through: :order_details
 
   # acvive_strage
   has_one_attached :image
