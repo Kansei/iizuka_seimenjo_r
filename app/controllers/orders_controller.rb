b9ce5c1d58ef6b2abdc3d42c0cd75bd986228863
Kansei Kogami <k_kansei_k@yahoo.co.jp>
2018-10-13 05:08:20 +0900

    create: orders controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,26 @@
+class OrdersController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @menus = Menu.all
+    @order
+  end
+
+  def confirm
+
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
