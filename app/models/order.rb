adf364f7be26d0fddfe51e8ca26877507e88957b
Kansei Kogami <k_kansei_k@yahoo.co.jp>
2018-10-13 11:39:27 +0900

    add: valitdasion for integer

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
-  has_many :order_details
+  has_many :order_details, dependent: :delete_all
   has_many :menus, through: :order_details
+
+  validates :total_price, numericality: {only_integer: true, greater_than: 0}
 end
 
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,6 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :menu
+
+  validates :quantity, numericality: {only_integer: true, greater_than: 0}
 end
