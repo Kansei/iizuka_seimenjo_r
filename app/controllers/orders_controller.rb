b0e258e8ff58232b1f8b8ea84d5d73dd21e57de3
Kansei Kogami <k_kansei_k@yahoo.co.jp>
2018-10-13 11:40:11 +0900

    done: create action on orders contrlloer

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,19 +8,22 @@ class OrdersController < ApplicationController
   end
 
   def confirm
+    orders = params[:order]
+
     @orders = []
     order_ids = []
 
-
-    params[:order].each do |order|
-      next if order[:quantity].blank?
-      order = {menu_id: order[:menu_id],name: nil, price: nil, quantity: order[:quantity].to_i}
+    orders.each do |order|
+      quantity = order[:quantity].to_i
+      next if quantity.blank? ||quantity < 1
+      order = {menu_id: order[:menu_id], name: nil, price: nil, quantity: quantity}
 
       @orders.append(order)
       order_ids.append(order[:menu_id])
     end
 
     if order_ids.empty?
+      flash[:danger] = "ちゃんと数を入力して。次はないよ。"
       redirect_to new_order_path
       return
     end
@@ -37,6 +40,23 @@ class OrdersController < ApplicationController
   end
 
   def create
+    ActiveRecord::Base.transaction do
+      order = Order.new(total_price: params[:total_price], status: "doing", number: rand(1000))
+      order.save!
+
+      @order_number = order.number
+
+      details = params[:order_details]
+      details.each do |detail|
+        order.order_details.create!(menu_id: detail[:menu_id], status: false, quantity: detail[:quantity])
+      end
+    end
+
+    render :recive and return
+
+  rescue ActiveRecord::RecordInvalid
+    flash.now[:danger] = "注文の受付に失敗しました。"
+    render :recive
   end
 
   def edit
@@ -47,4 +67,5 @@ class OrdersController < ApplicationController
 
   def destroy
   end
+
 end
