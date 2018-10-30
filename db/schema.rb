39d5e6480a111fa4f99520a2b4b6c7239b4cdd63
Kansei Kogami <kk7221dev@gmail.com>
2018-10-31 07:38:25 +0900

    add: invisible column of menu

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2018_10_25_201808) do
   create_table "menus", force: :cascade do |t|
     t.string "name"
     t.integer "price"
+    t.boolean "visible"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
